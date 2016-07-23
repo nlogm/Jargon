f525cb664ea7e74573545e8efdf0a06f97ada038
Luke Roche <lr98lr@aol.com>
2016-07-23 02:59:13 -0400

    MORE JOINTSgit add -Agit add -A!

---
+++ core/src/com/engine/joints/attributes/EnumController.java
@@ -0,0 +1,16 @@
+package com.engine.joints.attributes;
+
+public enum EnumController{
+	
+	NoteBook(JointedAttributes.DistanceAttributes.Damping_Ratio);
+
+	public enum States{
+		
+	}
+
+	
+	private EnumController(JointedAttributes.DistanceAttributes attr){
+		
+	}
+
+}
