9e3417e832f185af51b548cd6cff8d22b8bc8b6c
Luke Roche <lr98lr@aol.com>
2016-09-19 11:29:39 -0400

    Update LVL_Exception.java

--- core/src/com/engine/filemanager/parser/LVL_Exception.java
+++ core/src/com/engine/filemanager/parser/LVL_Exception.java
@@ -1,12 +1,22 @@
 package com.engine.filemanager.parser;
 
+/**
+ * Error handling for all syntax, casts, types, and index exceptions found
+ * within a .lvl format
+ * 
+ * @author Luke Roche 9/19/2016
+ */
+
 public class LVL_Exception extends Exception{
 
 	/**
-	 * 
+	 * serial
 	 */
 	private static final long serialVersionUID = -7410416287157296568L;
 	
+	/**
+	 * For syntax, throws message and quits
+	 */
 	public LVL_Exception(String message){
 		super(".lvl Syntax Error: " + message);
 	}
