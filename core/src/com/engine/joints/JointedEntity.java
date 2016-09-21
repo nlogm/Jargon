7d7d8412719094c29b215bdec02b099a0de2ecb6
Luke Roche <lr98lr@aol.com>
2016-09-21 09:32:15 -0400

    Update JointedEntity.java

--- core/src/com/engine/joints/JointedEntity.java
+++ core/src/com/engine/joints/JointedEntity.java
@@ -10,6 +10,15 @@ import com.badlogic.gdx.utils.Array;
 import com.engine.joints.attributes.JointType;
 import com.engine.joints.attributes.JointedAttributes;
 
+/**
+ *========================================================
+ *==  Jointed Entity id a very convenient way to        ==
+ *==  attach multiple entity's to a joint and           ==
+ *==     still be independent of one another            ==
+ *==                                                    ==
+ *== @author Luke Roche                                 ==
+ *========================================================
+                                                            */
 public class JointedEntity {
 
 	protected HashMap<String, Object> jointList;
@@ -26,6 +35,14 @@ public class JointedEntity {
 		jointAttributes = new HashMap<String, Object>();
 	}
 
+
+/**
+ *========================================================
+ *==          All set attributes are defined            ==
+ *==          and deprecated depending on the           ==  
+ *==          type of joint you chose                   == 
+ *========================================================
+ */
 	public void setAttribute(Object[] info, JointedAttributes.DistanceAttributes... attribute) {
 		int count = 0;
 		for (JointedAttributes.DistanceAttributes attr : attribute) {
@@ -33,44 +50,82 @@ public class JointedEntity {
 			jointAttributes.put(attr.name(), info[count++]);
 		}
 	}
-	
+	/**
+ *========================================================
+ *==          All set attributes are defined            ==
+ *==          and deprecated depending on the           ==  
+ *==          type of joint you chose                   == 
+ *========================================================
+ */
 	public void setAttribute(Object[] info, JointedAttributes.PrismaticAttributes... attribute) {
 		for (JointedAttributes.PrismaticAttributes attr : attribute) {
 			jointAttributes.put(attr.name(), info);
 		}
 	}
-	
+	/**
+ *========================================================
+ *==          All set attributes are defined            ==
+ *==          and deprecated depending on the           ==  
+ *==          type of joint you chose                   == 
+ *========================================================
+ */
 	public void setAttribute(Object[] info, JointedAttributes.RevoluteAttributes... attribute) {
 		for (JointedAttributes.RevoluteAttributes attr : attribute) {
 			jointAttributes.put(attr.name(), info);
 		}
 	}
-	
+	/**
+ *========================================================
+ *==          All set attributes are defined            ==
+ *==          and deprecated depending on the           ==  
+ *==          type of joint you chose                   == 
+ *========================================================
+                                                            */
 	public void setAttribute(Object[] info, JointedAttributes.RopeAttributes... attribute) {
 		for (JointedAttributes.RopeAttributes attr : attribute) {
 			jointAttributes.put(attr.name(), info);
 		}
 	}
-	
+	/**
+ *========================================================
+ *==          All set attributes are defined            ==
+ *==          and deprecated depending on the           ==  
+ *==          type of joint you chose                   == 
+ *========================================================
+ */
 	public void setAttribute(Object[] info, JointedAttributes.PulleyAttributes... attribute) {
 		for (JointedAttributes.PulleyAttributes attr : attribute) {
 			jointAttributes.put(attr.name(), info);
 		}
 	}
 	
+	/**
+	 * Assign the body for the joint
+	 */
 	public void addBodyA(Body bodyA){
 		joint.bodyA = bodyA;
 	}
+	/**
+	 * Assign the body for the joint
+	 */
 	public void addBodyB(Body bodyB){
 		joint.bodyB = bodyB;
 	}
+	/**
+	 * whether or not the bodies should be able to
+	 * collide with eachother
+	 */
 	public void isCollideConnected(boolean condition){
 		joint.collideConnected = condition;
 	}
 	
+	//dep
 	public void assignAttributes(){
 		
 	}
+	/**
+	 * Pop the found
+	 */
 	public void seek(String... name1) {
 		System.out.println(name1);
 		System.out.println(name1.toString());
